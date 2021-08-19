ea6933c077a2ab65bc887a30715c0c6b97e90d06
Chris Searcy <csearcy@ball.com>
2021-08-19 10:47:54 -0600

    remove unused using statements

--- ApplicationVersionComparator/Pages/VersionManager.cshtml.cs
+++ ApplicationVersionComparator/Pages/VersionManager.cshtml.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 using ApplicationVersionComparator.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
